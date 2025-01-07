ce275510d518a8fc7f0b0985e6a570fc1097e58a
Vaishu1118 <169073311+Vaishu1118@users.noreply.github.com>
2025-01-07 11:26:09 +0530

    swapping using temparory varibale

---
+++ demo016.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class demo016 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input numbers
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Before swapping: num1 = " + num1 + ", num2 = " + num2);
+
+        // Swapping using a temporary variable
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println("After swapping: num1 = " + num1 + ", num2 = " + num2);
+
+        scanner.close();
+    }
+}
