55b8c2f37f3d35a01949b2517647bbb8ef81cfa7
Vaishu1118 <169073311+Vaishu1118@users.noreply.github.com>
2025-01-27 11:05:16 +0530

    Do while user credential

--- demo008.java
+++ demo008.java
@@ -1,30 +1,16 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class demo008 {
+public class Demo008 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String correctUsername = "vaishnavi";
-        String correctPassword = "vaishu18";
-        String username, password;
-
-        System.out.println("Hello, welcome to Malla Reddy University!");
-        System.out.println("Please login here.");
-
-        do {
-            System.out.print("Enter Username: ");
-            username = scanner.nextLine();
-
-            System.out.print("Enter Password: ");
-            password = scanner.nextLine();
-
-            if (username.equals(correctUsername) && password.equals(correctPassword)) {
-                System.out.println("Successfully logged in!");
-                break;
-            } else {
-                System.out.println("Invalid username or password. Please try again.");
-            }
-        } while (true);
-
-        scanner.close();
+        Scanner sc = new Scanner(System.in);
+        String username,pass;
+        do{
+            System.out.println("Hello Mallareddy Unversity, Please Login: ");
+            System.out.print("Enter username: ");
+            username = sc.next();
+            System.out.print("Enter password: ");
+            pass = sc.next();
+        }while((!username.equals("vaishnavi"))||(!pass.equals("vaishu18")));
+        System.out.println("Successfully logged in.");
     }
 }
